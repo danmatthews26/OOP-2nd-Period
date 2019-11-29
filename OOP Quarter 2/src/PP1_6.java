5c9f82c46fcba6713064036ee836c34121db0b4d
Daniel Matthews <matthewsdan629@gmail.com>
2019-11-29 13:49:44 -0500

    Add files via upload

---
+++ OOP Quarter 2/src/PP1_6.java
@@ -0,0 +1,22 @@
+/******************************
+
+PP1_6
+
+Description: a program that prints the outline of a tree using asterisk (*) characters.
+
+Author: Daniel Matthews
+
+Date: 29 November 2019
+
+*****************************/
+public class PP1_6 {
+
+	public static void main(String[] args) {
+		System.out.println("    *");
+		System.out.println("   ***");
+		System.out.println("  *****");
+		System.out.println(" *******");
+		}
+
+	}
+
