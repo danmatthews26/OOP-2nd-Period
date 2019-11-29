cc0206cb0e619d167553ddb3e64a451cba33d244
Daniel Matthews <matthewsdan629@gmail.com>
2019-11-29 14:00:44 -0500

    Add files via upload

---
+++ OOP Quarter 2/src/OOP Quarter 2/src/PP1_6.java
@@ -0,0 +1,24 @@
+/******************************
+
+PP1_6
+
+Description: a program that prints the outline of a tree using asterisk (*) characters.
+
+Author: Daniel Matthews
+
+Date: 29 November 2019
+
+*****************************/
+public class PP1_6 {
+
+	public static void main(String[] args) {
+		
+		//prints each segment on a new line
+		System.out.println("    *");
+		System.out.println("   ***");
+		System.out.println("  *****");
+		System.out.println(" *******");
+		}
+
+	}
+
---
+++ OOP Quarter 2/src/OOP Quarter 2/src/PP1_7.java
@@ -0,0 +1,28 @@
+/******************************
+
+PP1_7
+
+Description: Write a program that prints a paragraph from a novel of your choice.
+
+Author: Daniel Matthews
+
+Date: 29 November 2019
+
+*****************************/
+
+
+public class PP1_7 {
+
+	public static void main(String[] args) {
+		
+		
+		//prints opening paragraph of Harry Potter and the Sorcerers Stone
+		System.out.println("Mr and Mrs Dursley, of number four, Privet Drive, were proud to\r\n" + 
+				"say that they were perfectly normal, thank you very much. They\r\n" + 
+				"were the last people you’d expect to be involved in anything\r\n" + 
+				"strange or mysterious, because they just didn’t hold with such\r\n" + 
+				"nonsense. ");
+
+	}
+
+}
